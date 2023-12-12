3439338c84e3c9741aa29b17fb5f4f8bdf03ec03
Vanessa Johnson <vmjohnson999@gmail.com>
2023-12-11 22:04:48 -0500

    progress

--- client.py
+++ client.py
@@ -10,22 +10,15 @@ def run():
         response = stub.CreatePost(
             reddit_pb2.Post(
                 postID= "1",
-                title= "Test Title",
-                text= "Test Text for Post",
-                mediaUrl= "http://google.com/media",
-                score= "100",
+                title= "Graduation 2023",
+                text= "The class for December 2023 will walk in May 2024",
+                mediaUrl= "http://december-grads.com",
+                score= "1000",
                 state= "NORMAL",
-                publicationDate= "12/05/23",
+                publicationDate= "12/09/23",
                 author= reddit_pb2.User(userID= "1"),
             )
     )
-        print("Post client received: " + str(response))
-        
-        response = stub.Retrieve(
-            reddit_pb2.Post(
-                postID= "1"
-            )
-        )
         print("Post client received: " + str(response))
 
         response = stub.UpVote(
@@ -37,10 +30,70 @@ def run():
 
         response = stub.DownVote(
             reddit_pb2.Post(
-                postID="1"
+                postID="46"
+            )
+        )
+        print("Post client received: " + str(response))
+
+
+        response = stub.Retrieve(
+            reddit_pb2.Post(
+                postID= "1"
+            )
+        )
+        print("Post client received: " + str(response))
+
+        response = stub.CreateComment(
+            reddit_pb2.Comment(
+                commentID= "30",
+                author= reddit_pb2.User(
+                    userID= "22"
+                ),
+                score= "-4",
+                state= "hidden",
+                publicationDate="12/06/23",
+                post= reddit_pb2.Post(
+                    postID= "1"
+                )
+            )
+        )
+        print("Post client received: " + str(response))
+
+        response = stub.CreateComment(
+            reddit_pb2.Comment(
+                commentID= "10"
+            )
+        )
+        print("Post client received: " + str(response))
+
+        response = stub.CreateComment(
+            reddit_pb2.Comment(
+                commentID= "46"
+            )
+        )
+        print("Post client received: " + str(response))
+
+        response = stub.UpVoteComment(
+            reddit_pb2.Comment(
+                commentID= "10"
             )
         )
         print("Post client received: " + str(response))
         
+        response = stub.DownVoteComment(
+            reddit_pb2.Comment(
+                commentID="12"
+            )
+        )
+        print("Post client received: " + str(response))
+
+
+        response = stub.ExpandCommentBranch(
+            reddit_pb2.Comment(
+                commentID="10"
+            )
+        )
+        print("Post client received: " + str(response))
+
 if __name__ == "__main__":
     run()
--- server.py
+++ server.py
@@ -7,8 +7,71 @@ import random
 
 class RedditOverAllService(reddit_pb2_grpc.RedditServicer):
    def __init__(self) -> None:
-       self.posts = dict()
-       self.comments = dict()
+       self.posts = {"103": reddit_pb2.Post(
+           
+       ),
+       "28": reddit_pb2.Post(
+           postID= "28",
+           title= "Cloud Infrastructure",
+           text="Cloud infrastructure was schedule for 7pm on Mondays and Wednesdays",
+           mediaUrl= "http://cloud-link.com",
+           score= "-50",
+           state="Normal",
+           publicationDate="11/08/23",
+       ),
+       "89": reddit_pb2.Post(
+            postID= "89",
+           title= "Responsible AI",
+           text="Responsible AI was on Mondays and Wednesdays at 2pm.",
+           mediaUrl= "http://responsible-ai.com",
+           score= "89",
+           state="Locked",
+           publicationDate="12/01/23",
+       ),
+       "46": reddit_pb2.Post(
+            postID= "46",
+           title= "Data Focused Python",
+           text="Data Focused Python was a remote class at 11am on Mondays and Wednesdays",
+           mediaUrl= "http://data-focused-python.com",
+           score= "-50",
+           state="Normal",
+           publicationDate="11/08/23",
+           author= reddit_pb2.User(
+               userID= "66"
+           )
+       )
+       }
+       self.comments = {"46": reddit_pb2.Comment(
+            commentID= "46",
+           score= "-50",
+           state="Normal",
+           publicationDate="11/08/23",
+           author= reddit_pb2.User(
+               userID= "32"
+           ),
+           postID= "89"
+       ),
+       "10": reddit_pb2.Comment(
+           commentID= "12",
+           author= reddit_pb2.User(
+               userID= "23"
+           ),
+           score= "11",
+           state= "Normal",
+           publicationDate= "04/01/23",
+           postID= "28", 
+       ),
+       "76": reddit_pb2.Comment(
+           commentID= "10",
+           author= reddit_pb2.User(
+               userID= "99"
+           ),
+           score= "44",
+           state= "Normal",
+           publicationDate= "04/01/23",
+           postID= "46", 
+       )
+       }
    def CreatePost(self, request, context):
        random_number = random.randint(0,1)
        if random_number == 1:
@@ -20,7 +83,20 @@ class RedditOverAllService(reddit_pb2_grpc.RedditServicer):
             score= request.score,
             state= request.state,
             publicationDate=request.publicationDate,
-            author= request.author
+            author= request.author,
+            comments= [
+        reddit_pb2.Comment(
+           commentID= "10",
+           author= reddit_pb2.User(
+               userID= "23"
+           ),
+           score= "11",
+           state= "Normal",
+           publicationDate= "04/01/23",
+           postID= "28", 
+       )
+            ],
+            text = "Awesome!"
            )
            self.posts[new_post.postID] = new_post
            return reddit_pb2.Post(
@@ -32,7 +108,20 @@ class RedditOverAllService(reddit_pb2_grpc.RedditServicer):
             state= request.state,
             publicationDate=request.publicationDate,
             author= request.author,
-            post_created_message = "Post was created"
+            post_created_message = "Post was created",
+            comments= [
+         reddit_pb2.Comment(
+           commentID= "10",
+           author= reddit_pb2.User(
+               userID= "23"
+           ),
+           score= "11",
+           state= "Normal",
+           publicationDate= "04/01/23",
+           postID= "28", 
+       )
+            ],
+            text= "Great work!"
             ) 
        else:
             return reddit_pb2.Post(
@@ -48,14 +137,20 @@ class RedditOverAllService(reddit_pb2_grpc.RedditServicer):
         )
            
    def UpVote(self, request, context):
-       post_type =  reddit_pb2.Post
-       comment_type = reddit_pb2.Comment
-       print(type(request) == post_type)
        request.upVote += 1
+       self.posts[request.postID] = request
+       return request
+   def UpVoteComment(self,request,context):
+       request.upVote += 1
+       self.posts[request.postID] = request
+       return request
+   def DownVoteComment(self,request,context):
+       request.downVote += 1
+       self.comments[request.commentID] = request
        return request
    def DownVote(self, request, context):
-       print(type(request))
        request.downVote += 1
+       self.comments[request.commentID] = request
        return request
    def Retrieve(self, request, context):
        if request.postID in self.posts.keys():
@@ -66,11 +161,23 @@ class RedditOverAllService(reddit_pb2_grpc.RedditServicer):
                text= "A post with that id does not exist"
            )
    def CreateComment(self, request, context):
-       return super().CreateComment(request, context)
+       request.was_created = True
+       self.comments[request.commentID] = request
+       return request
    def RetrieveListOfNMostUpvotedComments(self, request, context):
-       return super().RetrieveListOfNMostUpvotedComments(request, context)
+       number = request.number
+       post = request.post
+       comments = post.comments
+       print(comments)
+       sorted_comments = sorted(comments.items(), key=lambda comment: comment[1].upVote, reverse=True)
+       print(sorted_comments)
+       return sorted_comments[:number-1]
    def ExpandCommentBranch(self, request, context):
-       return super().ExpandCommentBranch(request, context)
+       number = request.number
+       comment = request.comment
+       comments = comment.replies
+       sorted_comments = sorted(comments.items(), key=lambda comment: comment[1].upVote, reverse=True)
+       return sorted_comments[:number-1]
 
 
 def serve():
